a04be62557b5444ee3fdfd793a87974e89a285ee
Marco Luglio <marcodejulho@gmail.com>
2020-05-28 19:38:40 -0300

    Initial Commit

---
+++ Weld/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Weld
+//
+//  Created by Marco Luglio on 28/05/20.
+//  Copyright © 2020 Marco Luglio. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
